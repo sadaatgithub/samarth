74513cddfe8dcb7186d80f2caff28357a5f9a158
sadaatgithub <sadachaudhari@gmail.com>
2023-02-19 11:42:00 +0530

    further improvement

--- frontend-samarth/pages/our-services/[slug].js
+++ frontend-samarth/pages/our-services/[slug].js
@@ -6,7 +6,16 @@ const eachService = [
   {
     id: 1,
     slug: "orthopedic-physiotherapy",
-    desc: `Orthopedic physiotherapy is a type of physical therapy that focuses on the treatment of musculoskeletal injuries and conditions. The goal of orthopedic physiotherapy is to help patients recover from injury, reduce pain and swelling, improve mobility, and prevent further injury.`
+    desc: `Orthopedic physiotherapy is a type of physical therapy that focuses on the treatment of musculoskeletal injuries and conditions. The goal of orthopedic physiotherapy is to help patients recover from injury, reduce pain and swelling, improve mobility, and prevent further injury.
+    /n Orthopedic physiotherapy can be used to treat a wide range of conditions, including sports injuries, joint pain, arthritis, and back pain. Physiotherapists use a variety of techniques, such as exercise, manual therapy, and modalities, to help patients recover from injury and improve their overall physical function.
+       
+       /n One of the main benefits of orthopedic physiotherapy is that it is non-invasive and does not involve the use of drugs or surgery. This makes it a safe and effective alternative to more invasive treatments and can help patients avoid the risks and side effects associated with these treatments.
+       
+      /n Orthopedic physiotherapy can be performed in a variety of settings, including hospitals, clinics, and rehabilitation centers. Physiotherapists work closely with patients to develop individualized treatment plans based on their specific needs and goals. This may involve a combination of exercises, manual therapy, and modalities, such as ultrasound, heat therapy, and electrical stimulation.
+       
+       /n In addition to helping patients recover from injury, orthopedic physiotherapy can also help prevent future injuries by promoting healthy posture, flexibility, and strength. This can be particularly beneficial for athletes and other individuals who are at risk of injury due to physical activity.
+       
+       /n In conclusion, orthopedic physiotherapy is an effective and non-invasive form of treatment for musculoskeletal injuries and conditions. It can help patients recover from injury, reduce pain, improve mobility, and prevent future injuries. If you are experiencing pain or injury, consider seeking the help of an orthopedic physiotherapist.`
   },
   {
     id: 2,
@@ -63,58 +72,7 @@ const Services = () => {
     <div className="">
       <PageBanner />
       <div className="flex mt-10  md:flex-row flex-col xl:px-20  px-6 mb-10">
-        {eachService
-          .filter((service) => service.slug == query.slug)
-          .map((service) => {
-            return (
-              <div className="flex gap-4 items-center flex-col lg:flex-row">
-                <div className="w-full md:w-1/2 ">
-                  <img
-                    className="w-full md:max-w-lg mb-8"
-                    src="https://via.placeholder.com/500x500"
-                    alt="Orthopedic Physiotherapy Image"
-                  />
-                </div>
-                <div className="w-full lg:w-1/2 ">
-                  <h2 className="text-3xl md:text-4xl font-bold leading-tight mb-4">
-                    {service.slug.split("-").join(" ").toUpperCase()}
-                  </h2>
-                  <p className="text-gray-700 text-xl mb-6">
-                    {service.desc}
-                  </p>
-                  <ul className="text-gray-700 text-lg mb-6">
-                    <li className="mb-4">
-                      <i className="fas fa-check text-green-500 mr-2"></i>
-                      Assessment and diagnosis of musculoskeletal conditions
-                    </li>
-                    <li className="mb-4">
-                      <i className="fas fa-check text-green-500 mr-2"></i>
-                      Personalized treatment plans to optimize recovery and
-                      improve mobility
-                    </li>
-                    <li className="mb-4">
-                      <i className="fas fa-check text-green-500 mr-2"></i>
-                      Rehabilitation exercises and techniques to manage pain and
-                      improve strength
-                    </li>
-                    <li className="mb-4">
-                      <i className="fas fa-check text-green-500 mr-2"></i>
-                      Education and guidance to help prevent future injuries and
-                      promote overall wellness
-                    </li>
-                  </ul>
-                  <Link
-                    href="#"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                  >
-                    Book an Appointment
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
-
-        {/* {eachService
+      {eachService
         .filter((service) => service.slug == query.slug)
         .map((service) => {
           return ( 
@@ -136,6 +94,8 @@ const Services = () => {
           );
         })}
 
+
+        {/* 
         <div className=" flex-col p-6  md:w-1/3 sticky top-4 left-0 hidden">
           <h4 className="text-xl text-rose-400 font-bold mt-10">Other Services</h4>
           <ul className=" flex flex-col gap-2 text-slate-600 mt-4 list-disc list-inside">
