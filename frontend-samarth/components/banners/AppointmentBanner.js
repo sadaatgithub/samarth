fc4bacecf97d24f3fa475c1c23bf3e81ae49579d
sadaatgithub <sadachaudhari@gmail.com>
2023-02-28 16:02:21 +0530

    social icons added

--- frontend-samarth/components/banners/AppointmentBanner.js
+++ frontend-samarth/components/banners/AppointmentBanner.js
@@ -8,7 +8,7 @@ const AppointmentBanner = () => {
   return (
     <section className="lg:px-14 px-4">
     <div className="h-52 w-full flex bg-gradient-to-l from-teal-500 to-teal-600 overflow-hidden mt-40 items-center justify-around rounded-lg md:flex-row flex-col relative">
-        <h3 className="text-xl text-center md:text-3xl w-2/3 md:font-bold text-gray-50 md:w-1/2 z-[1]">"Get back on track with expert physiotherapy to help you move, feel, and live better."</h3>
+        <h3 className="text-xl text-center md:text-3xl w-2/3 md:font-bold text-gray-50 md:w-1/2 z-[1]">&ldquo;Get back on track with expert physiotherapy to help you move, feel, and live better.&rdquo;</h3>
         <button
           className="flex max-w-xs justify-center items-center  gap-4 tracking-wide z-[1]
            text-lg bg-teal-800 hover:bg-white shadow-md hover:text-teal-500 group text-white px-9 py-4 rounded-sm  focus:bg-teal-800 transition-all duration-200"
