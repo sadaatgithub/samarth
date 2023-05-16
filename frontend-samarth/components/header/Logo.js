c568075d5c03c9dabb07f5cb6aedffa694169af5
sadaatgithub <sadachaudhari@gmail.com>
2023-05-16 09:22:03 +0530

    custom 404 page added

--- frontend-samarth/components/header/Logo.js
+++ frontend-samarth/components/header/Logo.js
@@ -7,7 +7,7 @@ const Logo = ({visible}) => {
     <div className={`${visible? "md:w-2/3 w-full flex-grow":"w-full"} text-xl md:text-3xl  font-bold flex col-span-full md:col-span-5 lg:col-span-4 text-slate-700 text-center items-center justify-start drop-shadow-sm`}>
       <Link href="/" className="flex items-center ">
       <div className="w-20 h-20">
-    <Image src={logoPng} width={80} height={80} alt="logo" className="-translate-x-5"/>
+    <Image src={logoPng} width={80} height={80} alt="logo" className="-translate-x-5 translate-y-1"/>
     </div>
      <h1 className="-translate-x-7 flex flex-col text-left text-teal-500  font-aladin font-black drop-shadow-sm leading-6"> Samartha <span className="text-gray-400 text-sm font-medium font-sourceSansPro">Physiotherapy & Rehab Clinic</span></h1>
      </Link></div>
---
+++ frontend-samarth/pages/404.js
@@ -0,0 +1,9 @@
+import Container from "@/components/container/Container";
+
+export default function Custom404() {
+    return <Container>
+        <div className="min-h-[50vh] grid place-items-center text-4xl text-gray-600 font-bold">
+        404 - Page Not Found
+        </div>
+        </Container>;
+  }
