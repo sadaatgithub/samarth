d40fc90a04efd276ddf6c3caa01dda0234ad53bb
sadaatgithub <sadachaudhari@gmail.com>
2023-05-19 10:09:11 +0530

    redirect paths added

--- frontend-samarth/next.config.js
+++ frontend-samarth/next.config.js
@@ -1,9 +1,68 @@
 /** @type {import('next').NextConfig} */
+
+
+// module.exports = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/about',
+//         destination: '/',
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/web/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/wp-includes/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/website/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/news/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/2019/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/wp1/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/wp2/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/cms/:slug*',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
+
